cab19b3de2f62a4ffdbaa12ae9f4920d6616f039
Rhattab0120 <randyhattab.one@gmail.com>
2021-02-07 12:47:06 -0500

    Added Routes

--- amazon-clone/src/app/app-routing.module.ts
+++ amazon-clone/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CheckoutComponent } from './pages/checkout/checkout.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'checkout', component: CheckoutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
