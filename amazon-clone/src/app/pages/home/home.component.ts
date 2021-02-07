9b8c2220a93ff5bb059c21a39c7f5798e5cd4003
Rhattab0120 <randyhattab.one@gmail.com>
2021-02-07 15:00:54 -0500

    Renamed service to services and generated shopping_cart service

--- amazon-clone/src/app/pages/home/home.component.ts
+++ amazon-clone/src/app/pages/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { ApiService } from 'src/app/service/api.service';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-home',
