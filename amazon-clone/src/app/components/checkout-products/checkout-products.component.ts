93349464e574c56ddcf8275ce9252d092bc6b1c1
Rhattab0120 <randyhattab.one@gmail.com>
2021-02-07 18:28:50 -0500

    Shopping Cart UI

--- amazon-clone/src/app/components/checkout-products/checkout-products.component.ts
+++ amazon-clone/src/app/components/checkout-products/checkout-products.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ShoppingCartService } from 'src/app/services/shopping-cart.service';
 
 @Component({
   selector: 'app-checkout-products',
@@ -8,7 +9,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class CheckoutProductsComponent implements OnInit {
   @Input() checkout_products: any[];
 
-  constructor() { }
+  constructor(private shopping_cart_service: ShoppingCartService) { }
 
   ngOnInit(): void {
     console.log('products: ', this.checkout_products)
