fb72eea1bc23c8066ffdb1055202d529bb5fce25
Rhattab0120 <randyhattab.one@gmail.com>
2021-02-07 15:42:13 -0500

    Add multiple products to cart

--- amazon-clone/src/app/services/shopping-cart.service.ts
+++ amazon-clone/src/app/services/shopping-cart.service.ts
@@ -4,11 +4,27 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ShoppingCartService {
+  shopping_cart_items: any[] = [];
 
   constructor() { }
 
   addProduct = (product) => {
-    console.log('Added: ', product)
-    localStorage.setItem('shopping_cart', product)
+    // console.log('Added: ', product)
+
+    let items = this.getShoppingCartItems();
+    if(items) {
+      items.push(product)
+      localStorage.setItem('shopping_cart', JSON.stringify(items))
+    } else {
+      this.shopping_cart_items.push(product);
+      localStorage.setItem('shopping_cart', JSON.stringify(this.shopping_cart_items))
+    }
+
+    // localStorage.setItem('shopping_cart', JSON.stringify(product))
+  }
+
+  getShoppingCartItems = () => {
+    let items = localStorage.getItem('shopping_cart')
+    return JSON.parse(items)
   }
 }
