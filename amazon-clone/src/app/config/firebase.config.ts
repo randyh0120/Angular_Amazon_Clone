34d5653d276bc4f96d0dc63716650c5eb5efe615
Rhattab0120 <randyhattab.one@gmail.com>
2021-02-07 20:34:15 -0500

    Before Update

---
+++ amazon-clone/src/app/config/firebase.config.ts
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyAXHzrB6s_U_kihzXwUu-lDuUCC6BFZLpw",
+  authDomain: "myclone-4fb1a.firebaseapp.com",
+  projectId: "myclone-4fb1a",
+  storageBucket: "myclone-4fb1a.appspot.com",
+  messagingSenderId: "957294118966",
+  appId: "1:957294118966:web:c6a34f1decd40fbc267097",
+  measurementId: "G-7BKEYF2E2H"
+};
